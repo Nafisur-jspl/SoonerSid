384e19a225e30865fe10d00c39a69cd2e4d802da
Adiamaan Keerthi <mak.adi55@gmail.com>
2017-02-01 22:51:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 
 # Mongo DB initialization
 client = MongoClient(os.environ["DB_ACCESS"])
-db = config["database"]["MONGO_DATABASE"];
-table = config["database"]["MONGO_TABLE"];
+#db = config["database"]["MONGO_DATABASE"];
+#table = config["database"]["MONGO_TABLE"];
 
 @app.route('/', methods=['GET'])
 def verify():
