871bd4883e2b3347c126e75c1f819522fbae7d45
adiamaan92 <mak.adi55@gmail.com>
2017-02-11 11:36:53 -0600

    Revert

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+import sys
+import json
+import pymongo
 import requests
 from flask import Flask, request
-from ChatHandler import ChatHandler
+from ConnectionEstablisher import ConnectionEstablisher
 
 app = Flask(__name__)
-chatter = ChatHandler()
+connection = ConnectionEstablisher()
 
 
 @app.route('/', methods=['GET'])
@@ -17,15 +20,21 @@ def verify():
         return request.args["hub.challenge"], 200
 
     return "Hello world", 200
-    
+
 
 @app.route('/', methods=['POST'])
-def web_hook():
+def webhook():
     # endpoint for processing incoming messaging events
+
     data = request.get_json()
+    log(request)  # logging request to see how facebook send back the request
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
     if data["object"] == "page":
+
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
+
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
@@ -33,8 +42,18 @@ def web_hook():
                         "id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    response_text = chatter.connection.api_connect(message_text)
-                    chatter.send_message(sender_id, response_text, requests)
+                    # Sending the message to API.AI logging and sending it back
+                    response_json = connection.api_connect(message_text)
+                    log(response_json)
+                    send_text = response_json['result']['fulfillment']['speech']
+                    log(send_text)
+                    send_message(sender_id, "got it, thanks!")  # To make sure the application is running
+                    send_message(sender_id, send_text)
+
+                    # Inserting the user into the db after check
+                    # Adding a Comment to check git integration
+                    if connection.dbrecord_exists(user_id=sender_id) is False:
+                        log(connection.dbrecord_insert(user_id=sender_id))
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -45,8 +64,35 @@ def web_hook():
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
                     pass
 
-
     return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
+
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    sys.stdout.flush()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
